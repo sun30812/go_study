a698b16a465acc7a0aec37a7f663ac8cf6926c52
sun30812 <sun30812@naver.com>
2021-08-10 10:10:09 +0900

    일부 디자인 요소 수정 및 readme.md 업데이트

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const app_port = 8080;
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || app_port);
 app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'index.html'));
@@ -16,4 +17,6 @@ app.get('/login', (req, res) => {
 app.get('/settings', (req, res) => {
 	res.sendFile(path.join(__dirname, 'settings.html'));
 })
-app.listen(app.get('port'));
+app.listen(app.get('port'), () => {
+	console.log(`현재 ${app.get('port')}번 포트에서 서버를 열었습니다. 포트는 서버의 환경변수를 통해 지정하는 것을 권장하며 테스트 용으로만 실행하는 경우 index.js 상단에 app_port상수를 수정하여 포트번호를 지정할 수 있습니다.`);
+});
