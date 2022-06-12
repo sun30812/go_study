4cf30f9c9cd32e52c92099d1c7b92ffa835f1ee8
sun30812 <sun30812@naver.com>
2022-06-12 10:51:36 +0900

    기능 추가 및 일부 디자인 수정
    * 학습 내역과 메뉴 분리
    * 업데이트 기록 보는 창 추가(개선 필요)

---
+++ public/main.js
@@ -0,0 +1,7 @@
+function releaseOn() {
+    document.getElementById("updateInfo").style.display = "block";
+}
+
+function releaseOff() {
+    document.getElementById("updateInfo").style.display = "none";
+}
